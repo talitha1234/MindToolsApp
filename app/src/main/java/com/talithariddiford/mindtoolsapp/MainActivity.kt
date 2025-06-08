d83aac8acb109a1d3f97daf83e75f8dc0f5fbe1f
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2025-06-08 13:20:06 +1000

    Add Mood Selection page

--- app/src/main/java/com/talithariddiford/mindtoolsapp/MainActivity.kt
+++ app/src/main/java/com/talithariddiford/mindtoolsapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.talithariddiford.mindtoolsapp
 
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,14 +25,24 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Add
-
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Tune
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 
 
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
@@ -59,45 +70,64 @@ fun MindToolsApp() {
 
 @Composable
 fun MindToolsPage(modifier: Modifier = Modifier) {
-    Column (
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-    ) {
-        MindToolsTopBar()
-        ActivityList()
+    Scaffold(
+        topBar = { MindToolsTopBar() },
+        bottomBar = { MindToolsBottomBar() },
+
+    ) { paddingValues ->
+        ActivityList(modifier = Modifier.padding(paddingValues))
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MindToolsTopBar() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Text(
-            text = stringResource(R.string.mind_tools),
-            style = TextStyle(
-                fontSize = 24.sp,
-                shadow = Shadow(color = Color.Black, blurRadius = 3f)
-            )
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        IconButton(onClick = { /* Add */ }) {
-            Icon(Icons.Rounded.Add, contentDescription = "Add")
+    TopAppBar(
+        title = {
+            Text(text = "Mind Tools")
         }
-        IconButton(onClick = { /* Search */ }) {
-            Icon(Icons.Rounded.Search, contentDescription = "Search")
+    )
+}
+
+@Composable
+fun MindToolsBottomBar() {
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = { /* TODO: Handle Add */ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = "Add"
+                )
+            }
+            IconButton(onClick = { /* TODO: Handle Search */ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "Search Activities"
+                )
+            }
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /* TODO: Handle filter */ }
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Tune,
+                    contentDescription = "Filter Activities"
+                )
+                Text(
+                    modifier = Modifier.padding(14.dp),
+                    text = "Filter by Mood"
+                )
+            }
         }
-        IconButton(onClick = { /* Filter */ }) {
-            Icon(Icons.Rounded.Tune, contentDescription = "Filter")
-        }
-    }
+    )
 }
 
 
+
+
 @Composable
-fun ActivityList() {
+fun ActivityList(modifier: Modifier = Modifier) {
     LazyVerticalGrid(
         modifier = Modifier
             .fillMaxSize()
@@ -105,7 +135,7 @@ fun ActivityList() {
             .padding(8.dp),
         columns = GridCells.Fixed(1)
     ) {
-        items(100) {
+        items(30) {
             Text(
                 modifier = Modifier
                     .padding(30.dp),
@@ -153,6 +183,103 @@ fun MoodSelectionPage(modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun MoodSelectionPage() {
+    Scaffold(
+        topBar = { MoodSelectionTopBar() },
+        bottomBar = {MoodSelectionBottomBar()},
+
+        ) { paddingValues ->
+        MoodList(modifier = Modifier.padding(paddingValues))
+    }
+}
+
+@Composable
+fun MoodList(modifier: Modifier = Modifier) {
+    val checkedStates = remember { mutableStateMapOf<Int, Boolean>() }
+
+    LazyVerticalGrid(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+            .padding(8.dp),
+        columns = GridCells.Fixed(1)
+    ) {
+        items(8) { index ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Mood $index",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        shadow = Shadow(
+                            color = Color.Unspecified, blurRadius = 3f)
+                    ),
+                    modifier = Modifier.weight(1f)
+                )
+                Checkbox(
+                    checked = checkedStates.getOrDefault(index, false),
+                    onCheckedChange = { checked ->
+                        checkedStates[index] = checked
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MoodSelectionTopBar() {
+    TopAppBar(
+        title = {
+            Text(text = "Mood Filter")
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* handle back click */ }) {
+                Icon(Icons.AutoMirrored.Rounded.ArrowBack, contentDescription = "Back")
+            }
+        }
+    )
+}
+
+@Composable
+fun MoodSelectionBottomBar() {
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = { /* TODO: Handle Home */ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Home,
+                    contentDescription = "Home"
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO: Handle Save */ }
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Check,
+                    contentDescription = "Save Selection"
+                )
+            }
+        }
+    )
+}
+
+
+//@Composable
+//fun AddActivityPage() {
+//
+//}
+
+@Composable
 @Preview(showBackground = true)
 fun MoodSelectionPagePreview() {
     MoodSelectionPage()
@@ -163,3 +290,4 @@ fun MoodSelectionPagePreview() {
 fun MindToolsPagePreview(){
     MindToolsPage()
 }
+
